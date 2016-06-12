40eb6bd9df3ecc55cf4b66f29e7e3925f7d26846
Gilles Robin <gillesr@webmails.com>
2016-06-12 20:47:13 +0200

    add support for ogg, aac and wav in AT

--- js/adapt-sound-manager.js
+++ js/adapt-sound-manager.js
@@ -17,20 +17,34 @@ define(function(require) {
             "click .sound-manager-extension-button": "onSoundManagerClick"
         },
 
-        preRender: function() {},
+        preRender: function() {
+        },
 
         render: function () {
             // Convert model data into JSON
             var data = this.model.toJSON();
             var template = Handlebars.templates["sound-manager"];
-            
+
             //load sounds(s)
             var sndArray = this.model.get('_sound-manager').sounds;
             this.soundsList = new Array();
             for(var i=0;i<sndArray.length;i++) {
-                this.soundsList.push(new Buzz.sound(sndArray[i].src));
+                var tmpArray = new Array();
+                if((Buzz.isOGGSupported())&&(sndArray[i].ogg!=undefined)&&(sndArray[i].ogg!='')){
+                    tmpArray.push(sndArray[i].ogg);
+                }
+                if((Buzz.isMP3Supported())&&(sndArray[i].mp3!=undefined)&&(sndArray[i].mp3!='')){
+                    tmpArray.push(sndArray[i].mp3);
+                }
+                if((Buzz.isWAVSupported())&&(sndArray[i].wav!=undefined)&&(sndArray[i].wav!='')){
+                    tmpArray.push(sndArray[i].wav);
+                }
+                if((Buzz.isAACSupported())&&(sndArray[i].aac!=undefined)&&(sndArray[i].aac!='')){
+                    tmpArray.push(sndArray[i].aac);
+                }
+                this.soundsList.push(new Buzz.sound(tmpArray, {preload:true, webAudioApi:true}));
             }
-
+                        
             this.$el.html(template(data)).appendTo($('.' + this.model.get('_id')));
             _.defer(_.bind(this.postRender, this));
         },
@@ -41,24 +55,14 @@ define(function(require) {
 
         inview: function(event, visible, visiblePartX, visiblePartY) {
             if (visible) {
-                if (visiblePartY === 'top') {
-                    this._isVisibleTop = true;
-                } else if (visiblePartY === 'bottom') {
-                    this._isVisibleBottom = true;
-                } else {
-                    this._isVisibleTop = true;
-                    this._isVisibleBottom = true;
-                }
-                if (this._isVisibleTop && this._isVisibleBottom) {
-                    if(this.model.get('_sound-manager').autoplay){
-                        if(!this.alreadyPlayed) {
-                           this.playAudio();
-                           if(this.model.get('_sound-manager').onlyOnce){
-                                this.alreadyPlayed = true;
-                           }
+                if(this.model.get('_sound-manager').autoplay){
+                    if(!this.alreadyPlayed) {
+                       this.playAudio();
+                       if(this.model.get('_sound-manager').onlyOnce){
+                            this.alreadyPlayed = true;
                        }
                    }
-                }
+               }
             }
         },
 
@@ -71,7 +75,7 @@ define(function(require) {
             var self = this;
             this.soundsList.forEach(function(element, index) {
                 if(index<self.soundsList.length-1){
-                    element.bind('ended', function(e) {
+                    element.bindOnce('ended', function(e) {
                         self.soundsList[index+1].play();
                     });
                 }
@@ -88,4 +92,4 @@ define(function(require) {
             });
         }
     });
-});
+});1
